369e366c2fa14c75df15b42225d5e6aa5ce4a08b
Brian Falor <brian.falor@gmail.com>
2016-08-26 22:37:42 -0600

    Fixed the code to have a better implementation with the write to LED. I also fixed it so you only need to change how many LEDS you want to use once, and it will work with the rest of the code. I added in an array for the 4x4 grid that I want to use for the reverb pedal. Lastly I fixed the delay and shut off LEDS part of the code.

--- Reverb_Pedal.ino
+++ Reverb_Pedal.ino
@@ -1,7 +1,6 @@
 #include <Adafruit_NeoPixel.h>
-#define PEDAL_BUTTON 0
 #define PIN 4 //Pin for NeoPixels 
-#define NUM_OF_LEDS 4
+#define NUM_OF_LEDS 16
 
 //Max pot values for 3.3v will be 675
 #define MAX_POT 675
@@ -11,22 +10,23 @@
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_OF_LEDS, PIN, NEO_GRB + NEO_KHZ800);
 
+int pattern[16]= {0,1,2,3,4,11,12,13,14,15,8,7,6,5,10,9};
 int Knob1 = A0;  // Left Pot
 int Knob2 = A1;  // Right Pot
-
 int max_pot = MAX_POT;
 
 unsigned int Knob1_new = 0; // the latest pot value being read in
 unsigned int Knob2_new = 0;
 unsigned int Knob1_old = 0; // the old pot value which will be compared with the new value.
 unsigned int Knob2_old = 0; //     I'm looking to see if there is a difference between these 
-unsigned int Button_Press = 0;//      In order to determine if I should change the display or not.
-unsigned int num = 0; 
-unsigned int counter = 0; 
-bool change; 
+unsigned int LED_num = 0;
+unsigned int counter = 0;
+unsigned int counter2 = 0;
+float step_amount = (max_pot/NUM_OF_LEDS);
+int display_time = 150;
 
 //I'm using this value for now just to keep the light somewhat dim while I proto this
-float myIntensity = .02; //2% brightness 
+float myIntensity = .04; //4% brightness 
 
 typedef struct {
   int R;
@@ -37,7 +37,6 @@ typedef struct {
 RGB color;
 
 void setup(){
-  pinMode(PEDAL_BUTTON, INPUT);
   color = {0, 0, 0};
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
@@ -49,76 +48,55 @@ void loop() {
   Knob2_Input();
   SetPixels();
   Time_delay();
-  delay(10);
+  Serial.print("Counter = ");
+  Serial.println(counter);
+  Serial.print("Counter2 = ");
+  Serial.println(counter2);
+  delay(5);
 }
 
 void Knob1_Input(){
-  Knob1_new = 0.9 * Knob1_new + 0.1 * analogRead(Knob1);
+  Knob1_new = (0.9 * Knob1_new + 0.1 * analogRead(Knob1));    //This funny looking equation helps smooth out the Pot values so they are stable.
     if(Knob1_new != Knob1_old){
-        change = true; // Change is registered 
-        counter = 0;
-       }
-
-   if(Knob1_new == Knob1_old){
-    change = false;
-   }
-   
-      if (change == true) {
-        counter = 0;
-         if(Knob1_new <= 15)                                                           { Off();}
-             else if((Knob1_new > 15)             && (Knob1_new <= (max_pot/4)))       { Blue(); num = 1;}
-             else if((Knob1_new > (max_pot/4))   && (Knob1_new <= (max_pot/2)))        { Blue(); num = 2;}
-             else if((Knob1_new > (max_pot/2))   && (Knob1_new <= (max_pot/4)*3))      { Blue(); num = 3;}
-             else if((Knob1_new > (max_pot/4)*3) && (Knob1_new <= (max_pot)))          { Blue(); num = 4;}  
+      for (int k = 1; k <= NUM_OF_LEDS; k++){
+           if(Knob1_new <= step_amount) { Off();}  //Pot value for the very first step, if condition is met, the the whole set of lights are off
+           else if((Knob1_new > (step_amount*k)) && (Knob1_new <= (step_amount*(k+1)))) { Blue(); LED_num = k;}  //Go through each possible step, and compare it with pot value, and turn on appropriate LED
       }
+        counter = 0;      //If there is a change, the counter is reset
+    }
 
-      else {
-         counter++; 
-      }
-
-   Knob1_old = Knob1_new;     
+    else{ //If pots aren't moved, then do nothing, except increase the counter for the time off delay. 
+       counter++; 
+    }
+ Knob1_old = Knob1_new;
 }
 
 
 void Knob2_Input(){
-  Knob2_new = 0.9 * Knob2_new + 0.1 * analogRead(Knob2);
+  Knob2_new = (0.9 * Knob2_new + 0.1 * analogRead(Knob2));    //This funny looking equation helps smooth out the Pot values so they are stable.
     if(Knob2_new != Knob2_old){
-        change = true; // Change is registered 
-        counter = 0;
-       }
-
-   if(Knob2_new == Knob2_old){
-    change = false;
-   }
-   
-      if (change == true) {
-        counter = 0;
-         if(Knob2_new <= 15)                                                           { Off();}
-             else if((Knob2_new > 15)             && (Knob2_new <= (max_pot/4)))       { Green(); num = 1;}
-             else if((Knob2_new > (max_pot/4))   && (Knob2_new <= (max_pot/2)))        { Green(); num = 2;}
-             else if((Knob2_new > (max_pot/2))   && (Knob2_new <= (max_pot/4)*3))      { Green(); num = 3;}
-             else if((Knob2_new > (max_pot/4)*3) && (Knob2_new <= (max_pot)))          { Green(); num = 4;}  
+      for (int k = 1; k <= NUM_OF_LEDS; k++){
+           if(Knob2_new <= step_amount) { Off();}  //Pot value for the very first step, if condition is met, the the whole set of lights are off
+           else if((Knob2_new > (step_amount*k)) && (Knob2_new <= (step_amount*(k+1)))) { Green(); LED_num = k;}  //Go through each possible step, and compare it with pot value, and turn on appropriate LED
       }
+        counter2 = 0;      //If there is a change, the counter is reset
+    }
 
-      else {
-         counter++; 
-      }
-
-   Knob2_old = Knob2_new;     
+    else{ //If pots aren't moved, then do nothing, except increase the counter for the time off delay. 
+       counter2++; 
+    }
+ Knob2_old = Knob2_new;
 }
 
 void Time_delay() {
-  if( counter == 250){
-    for (int j = 0; j <= 4; j++){
+  if((counter == display_time)||(counter2 == display_time)){
+    for (int j = 0; j <= NUM_OF_LEDS; j++){
         Off();
-        num = j;
+        LED_num = j;
         SetPixels();
-      }
-      
+      }  
       counter = 0;
-      change = false;
-      Serial.println("Reset Counter");
-      delay(800);
+      counter2 = 0;
    }
 }
 
@@ -131,18 +109,18 @@ void Green() {
 }
 
 void Off(){
-  color.R = color.G = color.B = 0; num = 0;
+  color.R = color.G = color.B = 0;
 }
 
 void SetPixels(){
-  SetLED(strip.Color(color.R * myIntensity, color.G * myIntensity, color.B * myIntensity), 0, num); 
+  SetLED(strip.Color(color.R * myIntensity, color.G * myIntensity, color.B * myIntensity), 0, LED_num); 
 }
 
 void SetLED(uint32_t c, uint8_t wait, int x) {
   int y = x;
-  for (uint16_t i = 0; i <= x; ++i) {
-    strip.setPixelColor(i, c);
-    strip.setPixelColor(y, strip.Color(0,0,0));   
+  for (uint16_t i = 0; i <= x; i++) {
+    strip.setPixelColor(pattern[i], c);
+    strip.setPixelColor(pattern[y], strip.Color(0,0,0));   
     strip.show();
   }
 }
